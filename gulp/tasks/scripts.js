9de088e152862f956f3848e91b0940606837d985
Vladimir Abramyants <vabramanc@gmail.com>
2019-06-07 18:05:27 +0300

    Create folder for gulp preset

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,25 @@
+module.exports = () => {
+    $.gulp.task('scripts:lib', () => {
+        return $.gulp.src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js'
+        ])
+            .on('error', console.log)
+            .pipe($.glp.concat('libs.min.js'))
+            .pipe($.gulp.dest('dist/js'))
+            .pipe($.bs.reload({
+                stream: true
+            }));
+    });
+    
+    $.gulp.task('scripts', () => {
+        return $.gulp.src([
+            'src/static/js/main.js',
+            'src/static/js/jquery.countdown.js'
+        ])
+            .pipe($.gulp.dest('dist/js'))
+            .pipe($.bs.reload({
+                stream: true
+            }));
+    });
+}
