56a4ea7cc89c426d3a15fe375e5484853bcdb097
Vladimir Abramyants <vabramanc@gmail.com>
2019-06-07 18:06:07 +0300

    Created control point for gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+global.$ = {
+    gulp: require('gulp'),
+    glp: require('gulp-load-plugins')(),
+    bs: require('browser-sync').create(),
+
+    path: {
+        tasks: require('./gulp/config/tasks.js')
+    }
+};
+
+$.path.tasks.forEach(taskPath => {
+    require(taskPath)()
+});
+
+// start gulp
+$.gulp.task('default', $.gulp.series(
+    $.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts'),
+    $.gulp.parallel('watch', 'serve')
+));
+
+// $.gulp.task('build', $.gulp.series(
+//     $.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts', 'img:build'),
+//     $.gulp.parallel('watch', 'serve')
+// ))
